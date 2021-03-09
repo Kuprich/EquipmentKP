724ae82ef791b517d236ddec091fdfaaa10459db
ilyaik <ilya-kyp@yandex.ru>
2021-03-09 09:31:28 +0300

    Реализована модель-представление редактирования оборудования посредтвом рефлексии

---
+++ EquipmentKP/Infrastructure/EventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquipmentKP.Infrastructure
+{
+    class EventArgs<T> : EventArgs
+    {
+        public T Arg { get; set; }
+        public EventArgs(T Arg) => this.Arg = Arg;
+        public static implicit operator EventArgs<T>(T arg) => new EventArgs<T>(arg);
+        public static implicit operator T(EventArgs<T> arg) => arg.Arg;
+    }
+}
--- EquipmentKP/ViewModels/EquipmentEditorViewModel.cs
+++ EquipmentKP/ViewModels/EquipmentEditorViewModel.cs
@@ -1,16 +1,49 @@
 ﻿using Equipment.Database.Entities;
+using EquipmentKP.Infrastructure;
+using EquipmentKP.Infrastructure.Command;
 using EquipmentKP.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows.Input;
 
 namespace EquipmentKP.ViewModels
 {
     class EquipmentEditorViewModel : ViewModelBase
     {
+        public event EventHandler<EventArgs<bool>> Complete;
+
+        private readonly Dictionary<string, object> _Values = new Dictionary<string, object>();
+        protected virtual bool SetValue(object value, [CallerMemberName] string Property = null)
+        {
+            if (_Values.TryGetValue(Property, out var old_value) && Equals(value, old_value))
+                return false;
+            _Values[Property] = value;
+            OnPropertyChanged(Property);
+            return true;
+        }
+        protected virtual T GetValue<T>(T Default, [CallerMemberName] string Property = null)
+        {
+            if (_Values.TryGetValue(Property, out var value))
+                return (T)value;
+            return Default;
+        }
+
+
         #region ПОЛЯ И СВОЙСТВА
 
         private readonly MainEquipment _Equipment;
+
+        #region string InventoryNo - Комплект оборудования --> Серийный номер
+
+        public string InventoryNo
+        {
+            get => GetValue(_Equipment.EquipmentsKit.InventoryNo);
+            set => SetValue(value);
+        } 
+        #endregion
 
         #region string Title - заголовок окна
 
@@ -25,9 +58,72 @@ namespace EquipmentKP.ViewModels
 
         #endregion
 
+        #region КОМАНДЫ
+
+        #region CancelCommand - Отмена изменений
+        private ICommand _CancelCommand = null;
+        public ICommand CancelCommand => _CancelCommand ??
+            new LambdaCommand(OnCancelCommandExecuted);
+        private void OnCancelCommandExecuted()
+        {
+            Reject();
+            Complete?.Invoke(this, false);
+        }
+        #endregion
+
+        #region RejectCommand - Сброс изменений
+        private ICommand _RejectCommand = null;
+        public ICommand RejectCommand => _RejectCommand ??
+            new LambdaCommand(OnRejectCommandExecuted, CanRejectCommandExecute);
+        private bool CanRejectCommandExecute(object p) => _Values.Count > 0;
+        private void OnRejectCommandExecuted(object p)
+        {
+            Reject();
+        }
+        #endregion
+
+        #region CommitCommand - Примение изменений
+        private ICommand _CommitCommand = null;
+        public ICommand CommitCommand => _CommitCommand ??
+            new LambdaCommand(OnCommitCommandExecuted, CanCommitCommandExecute);
+        private bool CanCommitCommandExecute(object p) => true;
+        private void OnCommitCommandExecuted(object p)
+        {
+            Commit();
+            Complete?.Invoke(this, false);
+        } 
+        #endregion
+
+        #endregion
+
         public EquipmentEditorViewModel(MainEquipment Equipment)
         {
             _Equipment = Equipment;
+        }
+
+        public void Commit()
+        {
+            var type = _Equipment.GetType();
+
+            foreach (var (propertyName, value) in _Values)
+            {
+                var property = type.GetProperty(propertyName);
+                if (property is null || property.CanWrite) continue;
+
+                property.SetValue(_Equipment, value);
+            }
+
+            _Values.Clear();
+        }
+        public void Reject()
+        {
+            var properties = _Values.Keys.ToArray();
+            _Values.Clear();
+
+            foreach (var property in properties)
+            {
+                OnPropertyChanged(property);
+            }
         }
 
         public EquipmentEditorViewModel() : this(new MainEquipment())
