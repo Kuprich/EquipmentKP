aad9addd9de07bb6805df6bbfe77a3d196bec0bd
ilyaik <ilya-kyp@yandex.ru>
2021-03-14 11:29:38 +0300

    создана разментка для быстрой фильтрации

---
+++ EquipmentKP/Infrastructure/Converters/EquipmentGroupItemsToCodeName.cs
@@ -0,0 +1,32 @@
+﻿using Equipment.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace EquipmentKP.Infrastructure.Converters
+{
+    class EquipmentGroupItemsToCodeName : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ReadOnlyObservableCollection<object> items)
+            {
+
+                var item = (MainEquipment)items.First();
+
+                return item.EquipmentsKit.Location.CodeName;
+            }
+
+            return "-";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException("Обратное преобразование невозможно!");
+        }
+    }
+}
