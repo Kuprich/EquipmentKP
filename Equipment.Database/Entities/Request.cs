e3f7646a0a9d4db7ebceb99062e5e9110a327f22
ilyaik <ilya-kyp@yandex.ru>
2021-03-25 23:04:17 +0300

    Перед попыткой создать сущность с документами иац

--- Equipment.Database/Entities/Request.cs
+++ Equipment.Database/Entities/Request.cs
@@ -7,8 +7,9 @@ namespace Equipment.Database.Entities
 {
     public class Request : Entity
     {
-        public int Number                   { get; set; }
-        public MainEquipment MainEquipment  { get; set; }
+        public int Number { get; set; }
+        public DateTime ReceiptDate { get; set; }
+        public MainEquipment MainEquipment { get; set; }
         public IList<RequestMovement> RequestMovements { get; set; } = new List<RequestMovement>();
     }
 }
