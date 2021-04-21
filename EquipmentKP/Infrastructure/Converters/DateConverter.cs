7baeb2de20e598870919c30fa95e5cd8f7a44edd
ilyaik <ilya-kyp@yandex.ru>
2021-04-21 23:42:14 +0300

    выгрузка в ветку мастер

---
+++ EquipmentKP/Infrastructure/Converters/DateConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace EquipmentKP.Infrastructure.Converters
+{
+    class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime val)
+                return val.ToShortDateString();
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (DateTime.TryParse((string)value, out DateTime val))
+                return val;
+            return DateTime.MinValue;
+        }
+    }
+}
